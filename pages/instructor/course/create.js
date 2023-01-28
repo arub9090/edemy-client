dd93d93628c2fba78c6d94092fe297e143b15ef1
ARIF RUBAYET <72675261+arub9090@users.noreply.github.com>
2023-01-27 20:51:56 -0500

    Removing the Development Text <pre>

--- pages/instructor/course/create.js
+++ pages/instructor/course/create.js
@@ -99,9 +99,6 @@ const CourseCreate = () => {
           handleImageRemove={handleImageRemove}
         />
       </div>
-      <pre>{JSON.stringify(values, null, 4)}</pre>
-      <hr />
-      <pre>{JSON.stringify(image, null, 4)}</pre>
     </InstructorRoute>
   );
 };
